038f765b8d41b011509e259abb93e4014d6cc69d
Waylon Dixon <wdixon@waylondixon.com>
2019-11-12 12:22:11 -0700

    added/implemented Hero class

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
   hero = 'Windstorm';
+  hero2: Hero = {
+    id: 1,
+    name: 'RufusMace'
+  };
 
   constructor() { }
 
